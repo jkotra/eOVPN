badfa1de3569cec77074f004966068f0a0742a7a
Jagadeesh Kotra <jagadeesh@stdin.top>
2021-02-25 11:04:20 +0530

    updata metainfo

--- eovpn/eovpn_base.py
+++ eovpn/eovpn_base.py
@@ -25,7 +25,7 @@ class Base:
     def __init__(self):
         self.APP_NAME = "eOVPN"
         self.APP_ID = "com.github.jkotra.eovpn"
-        self.APP_VERSION = "0.10"
+        self.APP_VERSION = "0.12"
         self.AUTHOR = "Jagadeesh Kotra"
         self.AUTHOR_MAIL = "jagadeesh@stdin.top"
         
